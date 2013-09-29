82b6dd7ec452f841cee29a841da8201a49a8ecf8
Aniello Del Sorbo <anidel@Melian.local>
2013-09-29 13:09:20 +0200

    little anims

--- android-user/src/com/joyn/kitchenchat/ui/LogIn.java
+++ android-user/src/com/joyn/kitchenchat/ui/LogIn.java
@@ -2,6 +2,8 @@ package com.joyn.kitchenchat.ui;
 
 import com.joyn.kitchenchat.R;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
@@ -13,7 +15,7 @@ import android.view.WindowManager;
 import android.view.View.OnClickListener;
 
 public class LogIn extends Activity {
-	private int SPLASH_TIME_OUT = 3000; // 5s
+	private int SPLASH_TIME_OUT = 1500;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -48,15 +50,43 @@ public class LogIn extends Activity {
 			
 		});
 		
+		final View facebook_button = findViewById (R.id.facebook);
+		final View twitter_button = findViewById (R.id.twitter);
+		
 		// Show the splash for a while, then launch the main activity
 		new Handler().postDelayed(new Runnable() {			 
             @Override
             public void run() {
-            	login_button.setVisibility(View.VISIBLE);
-            	signup_button.setVisibility(View.VISIBLE);
+            	login_button.setPivotX(login_button.getWidth()/2);
+            	login_button.setPivotY(login_button.getHeight()/2);
             	
-            	findViewById(R.id.facebook).setVisibility(View.VISIBLE);
-            	findViewById(R.id.twitter).setVisibility(View.VISIBLE);
+            	signup_button.setPivotX(login_button.getWidth()/2);
+            	signup_button.setPivotY(login_button.getHeight()/2);
+            	
+            	facebook_button.setPivotX(login_button.getWidth()/2);
+            	facebook_button.setPivotY(login_button.getHeight()/2);
+            	
+            	twitter_button.setPivotX(login_button.getWidth()/2);
+            	twitter_button.setPivotY(login_button.getHeight()/2);
+            	
+            	ObjectAnimator alpha1=ObjectAnimator.ofFloat(login_button, "alpha", 0f, 1f);
+                alpha1.setDuration(300);
+
+                ObjectAnimator alpha2=ObjectAnimator.ofFloat(signup_button, "alpha", 0f, 1f);
+                alpha2.setDuration(300);
+
+                ObjectAnimator alpha3=ObjectAnimator.ofFloat(facebook_button, "alpha", 0f, 1f);
+                alpha2.setDuration(300);
+
+                ObjectAnimator alpha4=ObjectAnimator.ofFloat(twitter_button, "alpha", 0f, 1f);
+                alpha2.setDuration(300);
+
+                AnimatorSet animset=new AnimatorSet();
+                animset.play(alpha1).after(250);
+                animset.play(alpha2).after(100);
+                animset.play(alpha3).after(150);
+                animset.play(alpha2);
+                animset.start();
             }
         }, SPLASH_TIME_OUT);
 
